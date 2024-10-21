d97fc8378ae7f3133e64f7a170196f355f6798c5
Miguel Zakharia <miguel.zakharia@gmail.com>
2024-10-21 14:06:05 -0400

    clean up and reorg

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,23 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
-
-git pull origin main;
-
 function doIt() {
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  brew bundle;
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	brew bundle # Install items in Brewfile
 
-	stow root-configs
-	stow nvim
-	stow bat
-	stow --target=~/Library/Application\ Support/ espanso
+	stow root-configs                  # Send those dotfiles to ~
+	stow --target=~/.config config-dir # Send configs to ~/.config
 
-  source ~/.zshrc;
+	source ~/.zshrc
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
+	doIt
 else
- read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
- echo "";
- if [[ $REPLY =~ ^[Yy]$ ]]; then
-	doIt;
- fi;
-fi;
-unset doIt;
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo ""
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
+unset doIt
